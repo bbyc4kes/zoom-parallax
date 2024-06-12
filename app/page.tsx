fff22839d545f89aa90e233cd2b5740b1908a18d
Maksym Azimov <anthoniotheruiner@gmail.com>
2024-06-12 19:40:37 +0200

    feature: add zoom parallax

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,23 @@
+'use client'
+
+import { useEffect } from 'react'
+import Lenis from '@studio-freight/lenis'
 import ZoomParallax from '@/components/zoom-parallax'
 
 export default function Home() {
+  useEffect(() => {
+    const lenis = new Lenis()
+
+    function raf(time: number) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+
+    requestAnimationFrame(raf)
+  }, [])
+
   return (
-    <main className="min-h-screen">
+    <main className="mt-[50vh] mb-[100vh]">
       <ZoomParallax />
     </main>
   )
